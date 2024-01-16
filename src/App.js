d1a5a8a7d9a8c595880a0bb026b5215a9a3cc22f
Parth Sinha <iitkgp.sinha.parth@gmail.com>
2024-01-16 14:31:56 +0530

    Adding modified files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useCallback, useEffect, useState, useRef } from 'react';
+import '../src/TailwindCss.css';
 function App() {
+  const [length, setLength] = useState(8);
+  const [numericpresent, setNumericPresent] = useState(false);
+  const [Characterpresent, setCharacterPresent] = useState(false);
+  const maxlen = 100;
+  const minlen = 1;
+  const [password, setPassword] = useState("");
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numericpresent) Alphabets += "0123456789";
+    if(Characterpresent) Alphabets += "~`!@#$%^&*()_-+=<>?,./][{}|";
+    for(let i = 1; i<= length; i++){
+      let character = Alphabets.charAt(Math.floor(Math.random()*(Alphabets.length))+1);
+      pass += character;  
+    }
+    setPassword(pass);
+  }, [length, numericpresent, Characterpresent, setPassword]);
+  useEffect(()=>{passwordGenerator()}, [length, numericpresent, Characterpresent, passwordGenerator])
+
+  // UseRef hook
+  const passwordRef = useRef(null)
+  const copyPasswordToClipboard = useCallback(()=>{
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  }, [password])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bg-black h-screen flex justify-center'>
+          <div className='bg-gray-800 h-fit w-fit mt-10 p-4 rounded-xl'>
+            <div className='text-center text-white text-3xl mb-4 shadow-md font-semibold'>
+              <h1>Password Generator</h1>
+            </div>
+            <div className=''>
+              <div className='flex'>
+                <input ref={passwordRef} readOnly value = {password} className = 'w-full rounded-lg shadow-black text-orange-700'>
+                </input>
+                <button className='bg-blue-800 rounded-md text-white w-fit py-1 px-2 mx-1' onClick={copyPasswordToClipboard}>Copy</button>
+              </div>
+              
+              <div className='p-3 text-gray-300'>
+                <input type='range' name='Length'className='' min = {minlen} max={maxlen} onChange={(e)=> setLength(e.target.value)}></input>
+                <label htmlFor = 'Length' className='m-4'>Length : {length}</label>
+                <input type='checkbox' name='Number' onChange={()=> setNumericPresent((numericpresent)=>!numericpresent)}/>
+                  <label htmlFor="Number" className='m-1' />number  
+                  <input type='checkbox' name='Character' onChange={()=> setCharacterPresent((Characterpresent)=>!Characterpresent)}/>
+                  <label htmlFor="Character" className='m-1' />Character
+              </div>
+            </div>
+              
+          </div>
+      </div>
+    </>
   );
 }
 
